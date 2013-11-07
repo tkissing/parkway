dc8e4b1562e35b3d2a8560549d089f14b95ccf56
Timo Kissing <timo.kissing@gmail.com>
2013-11-07 10:52:12 -0800

    Windows compatibility

--- index.js
+++ index.js
@@ -3,97 +3,96 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = (function() {
-	var noIndex = function(str) {
-		return String(str).replace(/\/index$/, '/').toLowerCase();
-	};
+    var noIndex = function(str) {
+        return String(str).replace(path.sep, '/', 'g').replace(/\/index$/, '/').toLowerCase();
+    };
 
-	var noSlash = function(str) {
-		return String(str).replace(/\/+$/, '');
-	}
+    var noSlash = function(str) {
+        return String(str).replace(path.sep, '/', 'g').replace(/\/+$/, '');
+    };
 
-	var trailingSlash  = function(str) {
-		return noSlash(str) + '/';
-	};
+    var trailingSlash  = function(str) {
+        return noSlash(str) + '/';
+    };
 
-	var defaultDelegate = function(path, filename){
-    	return filename[0] !== '.' && /\.(js|coffee)$/i.test(filename);
-  	};
+    var defaultDelegate = function(path, filename){
+        return filename[0] !== '.' && /\.(js|coffee)$/i.test(filename);
+    };
 
-  	// FIXME: support *
-  	var methods = ['get', 'post', 'put', 'delete', 'options'];
+    // FIXME: support *
+    var methods = ['get', 'post', 'put', 'delete', 'options'];
 
-  	var doMergeRoute = function(existingRoutes, routeModule, routeUrl) {
-  		var name = noSlash(noIndex(routeUrl)) || '/';
-  		var r = existingRoutes[name] || {};
-  		var s = {};
+    var doMergeRoute = function(existingRoutes, routeModule, routeUrl) {
+        var name = noSlash(noIndex(routeUrl)) || '/';
+        var r = existingRoutes[name] || {};
+        var s = {};
 
-  		_.each(routeModule, function(value, key) {
-  			if (methods.indexOf(key) > -1) {
-  				r[key] = value;
-  			} else {
-  				s[key] = value;
-  			}
-  		});
+        _.each(routeModule, function(value, key) {
+            if (methods.indexOf(key) > -1) {
+                r[key] = value;
+            } else {
+                s[key] = value;
+            }
+        });
 
-  		_.each(s, function(value, key) {
-  			doMergeRoute(existingRoutes, value, path.join(name, key));
-  		});
+        _.each(s, function(value, key) {
+            doMergeRoute(existingRoutes, value, path.join(name, key));
+        });
 
-		//console.log('domergeRoute', routeUrl, existingRoutes, r, s);
+        console.log('domergeRoute', routeUrl, existingRoutes, r, s);
 
-  		existingRoutes[name] = r;	
-  	}
+        existingRoutes[name] = r;
+    };
 
-  	var mergeRoute = function(existingRoutes, routePath, routeUrl) {
-		var mod = require(routePath);
+    var mergeRoute = function(existingRoutes, routePath, routeUrl) {
+        var mod = require(routePath);
 
-  		doMergeRoute(existingRoutes, mod, routeUrl);
-  	};
+        doMergeRoute(existingRoutes, mod, routeUrl);
+    };
 
-	var getRoutes = function(module, /*optional*/dir, /*optional*/urlPrefix, /*optional*/existingRoutes) {
-		var result = existingRoutes || {};
-		var files = [];
-		var subdirs = [];
-		
-		dir = trailingSlash(dir || 'routes');
-		urlPrefix = trailingSlash(urlPrefix || '');
+    var getRoutes = function(module, /*optional*/dir, /*optional*/urlPrefix, /*optional*/existingRoutes) {
+        var result = existingRoutes || {};
+        var subdirs = [];
 
-		var baseDir = path.dirname(module.filename);
-		var routeDir = path.join(baseDir, dir);
+        dir = trailingSlash(dir || 'routes');
+        urlPrefix = trailingSlash(urlPrefix || '');
 
-		fs.readdirSync(routeDir).forEach(function(filename) {
-			var name = filename.substring(0, filename.lastIndexOf('.'));
-			var joined = path.join(routeDir, filename);
+        var baseDir = path.dirname(module.filename);
+        var routeDir = path.join(baseDir, dir);
 
-			// console.log('joined', name, filename, joined);
+        fs.readdirSync(routeDir).forEach(function(filename) {
+            var name = filename.substring(0, filename.lastIndexOf('.'));
+            var joined = path.join(routeDir, filename);
 
-			if (fs.statSync(joined).isDirectory()) {
-				subdirs.push({dir: path.join(dir, filename), url: path.join(urlPrefix, filename)});
-			} else if (defaultDelegate(joined, filename)) {
-				mergeRoute(result, joined, [urlPrefix, name].join(''));
-			}
-    	});
+            // console.log('joined', name, filename, joined);
 
-    	subdirs.forEach(function(folder) {
-    		getRoutes(module, folder.dir, folder.url, result);
-    	});
+            if (fs.statSync(joined).isDirectory()) {
+                subdirs.push({dir: path.join(dir, filename), url: path.join(urlPrefix, filename)});
+            } else if (defaultDelegate(joined, filename)) {
+                mergeRoute(result, joined, [urlPrefix, name].join(''));
+            }
+        });
 
-		return result;
-	};
-	
-	var	installRoutes = function(module, app, /*optional*/dir, /*optional*/urlPrefix) {
-		var routes = getRoutes(module, dir, urlPrefix);
+        subdirs.forEach(function(folder) {
+            getRoutes(module, folder.dir, folder.url, result);
+        });
 
-		_.each(routes, function(value, key) {
-			_.each(value, function(handler, method) {
-				// console.log('registering ' + method + ' handler for ' + key, handler);
-				app[method](key, handler);
-			});
-		})
+        return result;
+    };
 
-		return routes;
-	};
-	
+    var	installRoutes = function(module, app, /*optional*/dir, /*optional*/urlPrefix) {
+        var routes = getRoutes(module, dir, urlPrefix);
 
-	return {get: getRoutes, install: installRoutes};
+        _.each(routes, function(value, key) {
+            _.each(value, function(handler, method) {
+                console.log('registering ' + method + ' handler for ' + key, handler);
+                app[method](key, handler);
+            });
+        });
+
+        return routes;
+    };
+
+
+    return {get: getRoutes, install: installRoutes};
 }());
